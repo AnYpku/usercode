cd3b1c4d506385dde93b3ebec7215438ef55b1c6
eavdeeva <>
2013-05-15 20:37:40 +0000

    first version

---
+++ WGammaAnalysis/remove_dot_so_d_tilda_files.sh
@@ -0,0 +1,26 @@
+cd CertifiedConstants
+rm *.so *.d *~
+cd ../
+
+cd Configuration
+rm *.so *.d *~
+cd ../
+
+cd Include
+rm *.so *.d *~
+cd ../
+
+cd Selection
+rm *.so *.d *~
+cd ../
+
+cd SkimSplitMerge
+rm *.so *.d *~
+cd ../
+
+cd QuickChecks
+rm *.so *.d *~
+cd ../
+
+
+rm *~
