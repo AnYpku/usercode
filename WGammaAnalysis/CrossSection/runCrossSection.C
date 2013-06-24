ca165cd11e6e794e5de14bf08374416ba9c917ab
eavdeeva <>
2013-06-24 23:10:05 +0000

    calc cross section

---
+++ WGammaAnalysis/CrossSection/runCrossSection.C
@@ -0,0 +1,22 @@
+{
+  gROOT->ProcessLine(".x ../Include/rootlogon.C");
+
+  gSystem->SetAclicMode(TSystem::kOpt);
+    //to run code in optimized mode; 
+    //doesn't really make it much faster...
+
+  TBenchmark time;
+  time.Start("time");
+  std::cout<<"CPU time = "<<time.GetCpuTime("time")<<", Real time = "<<time.GetRealTime("time")<<std::endl;  
+
+  //CalcAccAndEff accAndEff( TInputSample::MUON, "../Configuration/config.txt", 0, 1, 1);
+  CalcAccAndEff accAndEff( TInputSample::MUON);
+  accAndEff.LoopOverInputFiles();
+
+
+  time.Stop("time");
+  std::cout<<"CPU time = "<<time.GetCpuTime("time")<<", Real time = "<<time.GetRealTime("time")<<std::endl;  
+
+
+
+}
