be0c9bfbebfd179f50d151d309ae0978718628ba
Ekaterina Avdeeva <ekaterina.avdeeva@cern.ch>
2014-12-02 20:50:02 -0600

    to delete root files from WGammaOutput

---
+++ WGammaAnalysis/remove_output_root_files.sh
@@ -0,0 +1,11 @@
+rm WGammaOutput/MUON/VeryPreliminarySelected/*.root
+rm WGammaOutput/MUON/PreliminaryForTemplateMethodSelected/*.root
+rm WGammaOutput/MUON/FullySelected/*.root
+rm WGammaOutput/MUON/YieldsAndBackground/*.root
+rm WGammaOutput/MUON/Constants/*.root
+
+rm WGammaOutput/ELECTRON/VeryPreliminarySelected/*.root
+rm WGammaOutput/ELECTRON/PreliminaryForTemplateMethodSelected/*.root
+rm WGammaOutput/ELECTRON/FullySelected/*.root
+rm WGammaOutput/ELECTRON/YieldsAndBackground/*.root
+rm WGammaOutput/ELECTRON/Constants/*.root
